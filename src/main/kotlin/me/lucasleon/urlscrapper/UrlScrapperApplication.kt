62d3c4ce002ee921a820bab47b099ae967a01559
Lucas León <lucasleon2107@gmail.com>
2021-05-13 18:47:41 -0500

    Revert "Add cache"
    This reverts commit c4bbc1d28440bb148215684a4ad1fd753d02b7b4.

--- src/main/kotlin/me/lucasleon/urlscrapper/UrlScrapperApplication.kt
+++ src/main/kotlin/me/lucasleon/urlscrapper/UrlScrapperApplication.kt
@@ -2,9 +2,7 @@ package me.lucasleon.urlscrapper
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cache.annotation.EnableCaching
 
-@EnableCaching
 @SpringBootApplication
 class UrlScrapperApplication
 
