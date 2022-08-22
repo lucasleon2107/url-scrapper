fd61bda02b526bec7fd1ff7c02355a7fe3b3cee3
Lucas León <lucasleon2107@gmail.com>
2022-08-22 00:30:05 -0500

    Async scrape

--- src/main/kotlin/me/lucasleon/urlscrapper/services/ScrapperService.kt
+++ src/main/kotlin/me/lucasleon/urlscrapper/services/ScrapperService.kt
@@ -1,21 +1,22 @@
 package me.lucasleon.urlscrapper.services
 
 import it.skrape.core.htmlDocument
+import it.skrape.fetcher.AsyncFetcher
 import it.skrape.fetcher.HttpFetcher
-import it.skrape.fetcher.extractItBlocking
+import it.skrape.fetcher.extractIt
 import it.skrape.fetcher.skrape
 import me.lucasleon.urlscrapper.model.dtos.ScrapeResult
 import org.springframework.stereotype.Service
 
 @Service
 class ScrapperService {
-    fun scrapeUrl(urlToScrape: String): ScrapeResult {
-        return skrape(HttpFetcher) {
+    suspend fun scrapeUrl(urlToScrape: String): ScrapeResult {
+        return skrape(AsyncFetcher) {
             request {
                 url = urlToScrape
             }
 
-            extractItBlocking {
+            extractIt {
                 htmlDocument {
                     it.html = html
                 }
