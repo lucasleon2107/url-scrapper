65a2489e753ef3002cf1d582c258f54ea4ef4ead
Lucas León <lucasleon2107@gmail.com>
2022-08-22 00:59:51 -0500

    Remove unused import

--- src/main/kotlin/me/lucasleon/urlscrapper/services/ScrapperService.kt
+++ src/main/kotlin/me/lucasleon/urlscrapper/services/ScrapperService.kt
@@ -2,7 +2,6 @@ package me.lucasleon.urlscrapper.services
 
 import it.skrape.core.htmlDocument
 import it.skrape.fetcher.AsyncFetcher
-import it.skrape.fetcher.HttpFetcher
 import it.skrape.fetcher.extractIt
 import it.skrape.fetcher.skrape
 import me.lucasleon.urlscrapper.model.dtos.ScrapeResult
