ca95389e2e1a6cfd32aa512787817b8abb23428e
Lucas León <lucasleon2107@gmail.com>
2022-08-22 00:35:43 -0500

    Fix test

--- src/test/kotlin/me/lucasleon/urlscrapper/services/ScrapperServiceTest.kt
+++ src/test/kotlin/me/lucasleon/urlscrapper/services/ScrapperServiceTest.kt
@@ -1,7 +1,7 @@
 package me.lucasleon.urlscrapper.services
 
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -13,7 +13,7 @@ internal class ScrapperServiceTest {
     lateinit var scrapperService: ScrapperService
 
     @Test
-    fun `should scrape url` () = runBlocking{
+    fun `should scrape url`() = runBlocking {
         assertNotNull(scrapperService.scrapeUrl("https://www.google.com"))
     }
 }
